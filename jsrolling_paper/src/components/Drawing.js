da6c218c661d0730a789159a45f7153b43e4166b
KIMCW <kimcww@github.com>
2022-06-05 14:13:57 +0900

    toDataURL 확인

--- jsrolling_paper/src/components/Drawing.js
+++ jsrolling_paper/src/components/Drawing.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 // import { createRoot } from 'react-dom/client';
 import { Stage, Layer, Line, Text } from 'react-konva';
 import { Button, Grid } from '@mui/material'
+import axios from 'axios';
 
 export default function Drawing() {
   const [tool, setTool] = React.useState('pen');
   const [lines, setLines] = React.useState([]);
   const isDrawing = React.useRef(false);
+  const [inputId, setInputId] = React.useState('')
+  const [inputPw, setInputPw] = React.useState('')
 
+
+  const stageRef = React.useRef(null);
+  const handleExport = () => {
+    const uri = stageRef.current.toDataURL();
+    console.log(uri);
+  }
+
+  const handleImport = () => {
+    
+  }
   const handleMouseDown = (e) => {
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
@@ -35,11 +48,15 @@ export default function Drawing() {
   };
   const onSubmitDrawingData = () => {
 
+    axios.post('http://172.30.1.15:8080/??', { name: inputId, password: inputPw }).then(function (response) { console.log(response); }).catch(function (error) {
+      alert(error);
+    });
+
+
   }
   return (
     <div>
       <div>
-
         <Grid container justifyContent="flex-end">
           <select
             value={tool}
@@ -52,32 +69,34 @@ export default function Drawing() {
           </select>
           <Button id='submitDrawing' type='button' onSubmit={onSubmitDrawingData}>Submit</Button>
         </Grid>
-
       </div>
-
-      <Stage
-        width={window.innerWidth}
-        height={window.innerHeight}
-        onMouseDown={handleMouseDown}
-        onMousemove={handleMouseMove}
-        onMouseup={handleMouseUp}
-      >
-        <Layer>
-          {lines.map((line, i) => (
-            <Line
-              key={i}
-              points={line.points}
-              stroke="#000000"
-              strokeWidth={5}
-              tension={0.5}
-              lineCap="round"
-              globalCompositeOperation={
-                line.tool === 'eraser' ? 'destination-out' : 'source-over'
-              }
-            />
-          ))}
-        </Layer>
-      </Stage>
+      <Fragment>
+        <button onClick={handleExport}>Click here to log stage data URL</button>
+        <button onClick={handleImport}>Import data</button>
+        <Stage ref={stageRef}
+          width={window.innerWidth}
+          height={window.innerHeight}
+          onMouseDown={handleMouseDown}
+          onMousemove={handleMouseMove}
+          onMouseup={handleMouseUp}
+        >
+          <Layer>
+            {lines.map((line, i) => (
+              <Line
+                key={i}
+                points={line.points}
+                stroke="#000000"
+                strokeWidth={5}
+                tension={0.5}
+                lineCap="round"
+                globalCompositeOperation={
+                  line.tool === 'eraser' ? 'destination-out' : 'source-over'
+                }
+              />
+            ))}
+          </Layer>
+        </Stage>
+      </Fragment>
 
     </div>
   );

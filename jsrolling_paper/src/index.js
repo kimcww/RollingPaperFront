e09b97c895e3bfa40c63c4b3a3170721f9756724
KIMCW <kimcww@github.com>
2022-07-10 15:25:02 +0900

    회원가입 후 즉시 로그인 일부 구현

--- jsrolling_paper/src/index.js
+++ jsrolling_paper/src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import promiseMiddleware from "redux-promise";
+import reduxThunk from "redux-thunk";
+import  reducer from "./_reducers";
+
+const createStoreWidthMiddleware = applyMiddleware(
+  promiseMiddleware,
+  reduxThunk
+)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider
+    store={createStoreWidthMiddleware(
+        reducer,
+        //개발자 도구를 사용하기 위한 설정
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

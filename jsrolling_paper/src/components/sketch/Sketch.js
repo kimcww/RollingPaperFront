c135014806ab0c2fb5b3302c21ad953d778b2443
KIMCW <kimcww@github.com>
2022-08-14 13:55:39 +0900

    add drawing stage

--- jsrolling_paper/src/components/sketch/Sketch.js
+++ jsrolling_paper/src/components/sketch/Sketch.js
@@ -1,5 +1,4 @@
 import css from '../../css/sketch/Sketch.module.css';
-import PaintPalette from './PaintPalette';
 import Drawing from '../Drawing';
 
 export default function Sketch(){
@@ -8,7 +7,6 @@ export default function Sketch(){
     return (  
         <div id = 'sketchman' className={css.sketch}>
             <Drawing className={css.sketch} ></Drawing>
-            <PaintPalette></PaintPalette>
         </div>
     )
 };

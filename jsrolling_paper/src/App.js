f63c22e9f04127225958aa12ceb707edd17840cc
KIMCW <kimcww@github.com>
2022-08-14 15:47:27 +0900

    profile intro edit 기능 추가

--- jsrolling_paper/src/App.js
+++ jsrolling_paper/src/App.js
@@ -1,7 +1,7 @@
 import Main from './components/Main';
 import Home from './components/home/Home.js';
 import Title from './components/Title';
-
+import SignUp from './components/SignUp'
 function App() {
   return (
     //<Title titleName={"Changwoo"}/>

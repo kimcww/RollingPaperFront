ca02b1bf1b52c145f4610a2e113eebb103bce8ec
KIMCW <kimcww@github.com>
2022-10-23 14:34:44 +0900

    profile tooltip 추가

--- jsrolling_paper/src/components/profile/ProfileImg.js
+++ jsrolling_paper/src/components/profile/ProfileImg.js
@@ -2,6 +2,7 @@ import css from '../../css/profile/ProfileImg.module.css';
 import Popup from 'reactjs-popup';
 import { useState } from 'react';
 import ProfileImageSelecterModal from './ProfileImageSelecterModal';
+import ReactTooltip from 'react-tooltip'
 
 export default function ProfileImg() {
     const [open, setOpen] = useState(false);
@@ -15,7 +16,10 @@ export default function ProfileImg() {
     }
     return (
         <div className={css.profileImg}>
-            <img src={profileURL} className={css.sourceImg} onClick={openModal} ></img>
+            <img data-tip data-for="registerTip" src={profileURL} className={css.sourceImg} onClick={openModal} ></img>
+            <ReactTooltip id="registerTip" place="bottom" effect="solid" type = "info">
+                프로필을 변경하시려면 클릭
+            </ReactTooltip>
             <Popup id="" open={open} position="center center" onClose={closeModal} modal nested>
                 <ProfileImageSelecterModal closeEvent={closeModal} setProfileURL={setProfileURL}></ProfileImageSelecterModal>
             </Popup>

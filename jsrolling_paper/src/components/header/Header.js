df60a9ed2f3bf7b62a8c4fcd9b0453e83b48df37
KIMCW <kimcww@github.com>
2022-11-06 15:55:59 +0900

    우측 정렬, 로그인, 회원가입, 공유

--- jsrolling_paper/src/components/header/Header.js
+++ jsrolling_paper/src/components/header/Header.js
@@ -5,7 +5,7 @@ import css from '../../css/Header.module.css';
 import Login from '../Auth/Login';
 import Register from '../Auth/Register';
 
-export default function Header(){
+export default function Header() {
     const [loginOpen, setLoginOpen] = useState(false);
     const [registerOpen, setRegisterOpen] = useState(false);
     const user = useSelector((store) => store.user);
@@ -28,42 +28,37 @@ export default function Header(){
         <div className={css.header}>
             <div className={css.header_left}>
                 <h2>하다사 롤링페이퍼</h2>
-            </div>  
-            <div className={css.header_right}>
-            {
-                (function() {
-                    if(user){
-                        if (user.memberSeq > 0) return (<div>{`${user.memberNm} 님 환영합니다.`}</div>);
-                    }
-                })()
-            }
-
-        
-                <button className={css.basic_btn} type='button' name='login' onClick={openLoginModal}>로그인</button>
-                <button className={css.basic_btn} type='button' name='register' onClick={openRegisterModal}>회원가입</button>
-                <button className={css.basic_btn} type='button' name='share'>공유</button>
             </div>
-            <div className={css.actions}>
-                    <Popup 
+            <div className={css.header_right}>
+                {
+                    (function () {
+                        if (user) {
+                            if (user.memberSeq > 0) return (<div>{`${user.memberNm} 님 환영합니다.`}</div>);
+                        }
+                    })()
+                }
+                <button className={css.basic_btn} type='button' name='login' onClick={openLoginModal}>로그인
+                    <Popup
                         open={loginOpen}
                         onClose={closeLoginModal}
-                        modal 
-                        position = "center center"
+                        modal
+                        position="center center"
                         nested
                     >
-                        <Login closeEvent={closeLoginModal}/>
-                    </Popup>
-                    <Popup 
+                        <Login closeEvent={closeLoginModal} />
+                    </Popup></button>
+                <button className={css.basic_btn} type='button' name='register' onClick={openRegisterModal}>회원가입
+                    <Popup
                         open={registerOpen}
                         onClose={closeRegisterModal}
-                        modal 
-                        position = "center center"
+                        modal
+                        position="center center"
                         nested
                     >
-                        <Register closeEvent={closeRegisterModal}/>
-                    </Popup>
+                        <Register closeEvent={closeRegisterModal} />
+                    </Popup></button>
+                <button className={css.basic_btn} type='button' name='share'>공유</button>
             </div>
-
         </div>
     )
 };
